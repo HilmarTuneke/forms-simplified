2698d562009fdab3d188871f7356c0863a24f0d2
phhbr <github@phhbr.de>
2020-07-07 08:24:53 +0200

    fixed router
    (cherry picked from commit 2cedfb94c85f219d48f436d954416e104999a74b)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { DropdownComponent } from './dropdown/dropdown.component';
@@ -8,7 +7,6 @@ import { DropdownComponent } from './dropdown/dropdown.component';
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
     HttpClientModule,
     ReactiveFormsModule,
   ],
